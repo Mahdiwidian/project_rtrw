e599cd01fc9d60f3449f012af9fb4790349f7632
ridzqiawan.hammam <ridzqiawan.hammam@sirclo.com>
2022-12-23 06:34:14 +0700

    feat: add dynamic filter type page transaction

--- pages/transaksi/data-index.php
+++ pages/transaksi/data-index.php
@@ -1,13 +1,33 @@
 <?php
 include('../../config/koneksi.php');
 
-$query_trans = "SELECT * FROM transaksi";
-$query_filter = "SELECT * FROM transaksi WHERE type = 'pemasukan'";
-$hasil_trans = mysqli_query($db, $query_filter);
+// dynamic data transaksi
+if (isset($_POST['value'])) {
+  $type_trans = $_POST['value'];
+} else {
+  $type_trans = "";
+}
+
+switch ($type_trans) {
+  case "income":
+    $query_trans = "SELECT * FROM transaksi WHERE type = 'pemasukan'";
+    break;
+  case "outcome":
+    $query_trans = "SELECT * FROM transaksi WHERE type = 'pengeluaran'";
+    break;
+  default:
+    $query_trans = "SELECT * FROM transaksi";
+    break;
+}
+$hasil_trans = mysqli_query($db, $query_trans);
 $data_trans = array();
 
 while ($row = mysqli_fetch_assoc($hasil_trans)) {
   $data_trans[] = $row;
+}
+
+if (isset($_POST['value'])) {
+  echo json_encode($data_trans);
 }
 
 // hitung warga
--- pages/transaksi/index.php
+++ pages/transaksi/index.php
@@ -5,40 +5,89 @@
 <?php include('data-index.php') ?>
 
 <div style="margin: 26px;">
-  <form method="POST">
-    <select name="typeDropdown" id='typeDropdown' style="margin-right: 34px;width: 150px;">
+  <form method="POST" name="filterForm" id="filterForm" action="transaksi/data-index.php">
+    <select onchange="filter(this.value)" id='typeDropdown' style="margin-right: 34px;width: 150px;">
       <option value="all" selected>All</option>
       <option value="income">Pemasukan</option>
       <option value="outcome">Pengeluaran</option>
-    </select><input type="date" style="width: 150px;margin-bottom: 16px;" />
-    <p style="font-weight: bold;">12/18/2022</p>
-    <?php foreach ($data_trans as $trans) : ?>
-      <div class="panel panel-primary" style="margin-bottom: 8px;">
-        <div class="panel-body">
-          <div style="display: flex; justify-content: space-between;">
-            <h4>Rp <?php echo $trans['type'] == 'pemasukan' ? '' : '-' ?><?= $trans['nominal'] ?></h4><span style="<?php echo $trans['type'] == 'pemasukan' ? 'color: rgb(55,211,41)' : 'color: rgb(203,58,49)' ?>"><?= $trans['type'] ?></span>
+    </select><input type="date" onchange="filterDate(this.date)" style="width: 150px;margin-bottom: 16px;" />
+    <div id="showData">
+      <?php foreach ($data_trans as $trans) : ?>
+        <div class="panel panel-primary" style="margin-bottom: 8px;">
+          <div class="panel-body">
+            <div style="display: flex; justify-content: space-between;">
+              <h4>Rp <?php echo $trans['type'] == 'pemasukan' ? '' : '-' ?><?= $trans['nominal'] ?></h4><span style="<?php echo $trans['type'] == 'pemasukan' ? 'color: rgb(55,211,41)' : 'color: rgb(203,58,49)' ?>"><?= $trans['type'] ?></span>
+            </div>
+            <p class="panel-text"><?= $trans['description'] ?></p>
           </div>
-          <p class="panel-text"><?= $trans['description'] ?></p>
         </div>
-      </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
+    </div>
   </form>
 </div>
 
 <?php if (isset($_POST['typeDropdown'])) var_dump($_POST['typeDropdown']) ?>
 
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- <script>
   $('document').ready(function() {
-    $('#typeDropdown').on('change', function(e) {
-      if (isNaN(this.value))
-        // alert("Please enter a number", e);
-        console.log($data_trans)
-      else {
-        alert('value', $data_trans);
-      }
+    $('#typeDropdown').on('change', function() {
+      $('#filterForm').submit(), function(res) {
+        console.log(res)
+      };
     });
   });
 </script> -->
+<script>
+  function filter(item) {
+    $.ajax({
+      type: "POST",
+      url: "transaksi/data-index.php",
+      dataType: "JSON",
+      data: {
+        value: item
+      },
+      success: function(data) {
+        // console.log(data);
+        var html = '';
+        var i;
+        var valueMoney, classColor;
+        for (i = 0; i < data.length; i++) {
+          if (data[i].type == 'pemasukan') {
+            valueMoney = data[i].nominal
+            classColor = 'color: rgb(55,211,41)'
+          } else {
+            valueMoney = '-' + data[i].nominal
+            classColor = 'color: rgb(203,58,49)'
+          }
+
+          html += '<div class="panel panel-primary" style="margin-bottom: 8px;">' +
+            '<div class="panel-body">' +
+            '<div style="display: flex; justify-content: space-between;">' +
+            '<h4>Rp ' + valueMoney + '</h4>' +
+            '<span style="' + classColor + '">' + data[i].type + '</span>' +
+            '</div>' +
+            '<p class="panel-text">' + data[i].description + '</p>' +
+            '</div>' +
+            '</div>';
+        }
+        $('#showData').html(html);
+      }
+    });
+  }
+
+  // function filterDate(item) {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "transaksi/data-index.php",
+  //     data: {
+  //       date: date
+  //     },
+  //     success: function(data) {
+  //       console.log(data);
+  //     }
+  //   });
+  // }
+</script>
 
 <?php include('../_partials/bottom.php') ?>
