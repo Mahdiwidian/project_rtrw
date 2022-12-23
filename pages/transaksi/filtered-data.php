c6566c365dd716022d6c1cf0288c91b0c8531935
ridzqiawan.hammam <ridzqiawan.hammam@sirclo.com>
2022-12-23 19:44:10 +0700

    fix(transaksi): remove file and update code

--- pages/transaksi/filtered-data.php
+++ pages/transaksi/filtered-data.php
@@ -2,32 +2,25 @@
 include('../../config/koneksi.php');
 
 // dynamic data transaksi
-if (isset($_POST['value'])) {
-  $type_trans = $_POST['value'];
-} else {
-  $type_trans = "";
-}
+$type_trans = $_POST['type'];
+$date_trans = $_POST['date'];
 
 switch ($type_trans) {
-  case "income":
-    $query_trans = "SELECT * FROM transaksi WHERE type = 'pemasukan'";
-    break;
-  case "outcome":
-    $query_trans = "SELECT * FROM transaksi WHERE type = 'pengeluaran'";
-    break;
-  default:
-    $query_trans = "SELECT * FROM transaksi";
-    break;
+    case "income":
+        $query_trans = "SELECT * FROM transaksi WHERE type = 'pemasukan' AND date(created_at)='$date_trans'";
+        break;
+    case "outcome":
+        $query_trans = "SELECT * FROM transaksi WHERE type = 'pengeluaran' AND date(created_at)='$date_trans'";
+        break;
+    default:
+        $query_trans = "SELECT * FROM transaksi WHERE date(created_at)='$date_trans'";
+        break;
 }
 $hasil_trans = mysqli_query($db, $query_trans);
 $data_trans = array();
 
 while ($row = mysqli_fetch_assoc($hasil_trans)) {
-  $data_trans[] = $row;
+    $data_trans[] = $row;
 }
 
-if (isset($_POST['value'])) {
-  echo json_encode($data_trans);
-}
-
-?>
+echo json_encode($data_trans);
